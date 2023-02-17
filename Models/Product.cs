de20157239717ab2e1adb473d5eb37455ba47589
QuangDai97 <ngoquangdai100397@gmail.com>
2023-02-17 20:34:04 +0700

    update models

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,5 +12,6 @@ namespace api.Models
         public string Type { get; set; }
         public string Description { get; set; } 
         public decimal Price { get; set; } 
+        public int prices { get; set; }
     }
 }
